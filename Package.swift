476efd112ec21c86b34082d30f00769c2d67a38d
Callum McColl <callum.mccoll1@gmail.com>
2022-06-23 17:33:30 +1000

    Don't include docC plugin in windows

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@
 
 import PackageDescription
 
+#if os(Windows)
+let deps: [Package.Dependency] = []
+#else
+let deps: [Package.Dependency] = [.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")]
+#endif
+
 let package = Package(
     name: "GUUnits",
     products: [
@@ -11,10 +17,7 @@ let package = Package(
             name: "GUUnits",
             targets: ["GUUnits"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-    ],
+    dependencies: deps,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
